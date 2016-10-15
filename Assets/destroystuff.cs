4d462cac9118d184641e8d4e2a8a0aba8f68a5ef
ThanawatMynaf <tmynaf@uci.edu>
2016-10-15 16:50:12 -0700

    Add files via upload
    added destroying for passed object

---
+++ Assets/destroystuff.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroystuff : MonoBehaviour
+{
+
+    // Use this for initialization
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+}
