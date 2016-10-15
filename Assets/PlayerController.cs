4a578aa556a02369c4a95b6c6ae65b635cf2425e
CodiakVII <codiak759@gmail.com>
2016-10-15 14:47:42 -0700

    Added Player Controller (Movement Complete)

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,21 +4,67 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public float movement_h;
+	public float movement_v;
+	public float infection;
+	public bool isWrestling;
 
-	private Rigidbody2D rb;
+	public float beginTime;
+	public float wrestleTime;
+
+
+	private Rigidbody rb;
 
 	void Start ()
 	{
-		rb = GetComponent<Rigidbody2D>();
+		rb = GetComponent<Rigidbody>();
 	}
 
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
 
-		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		//if (isWrestling) {
+			//Wrestle ();
+		//}
 
-		rb.AddForce (movement * speed);
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			movement_h = 2 * speed;
+			
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			movement_h = -(speed / 5);
+
+		} else {
+			movement_h = 0.0f;
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			movement_v = 1.5f * speed;
+
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			movement_v = -1.5f * speed;
+
+		} else {
+			movement_v = 0.0f;
+		}
+
+		this.GetComponent<Rigidbody> ().velocity = new Vector2 (4.0f + movement_h, movement_v);
+
 	}
+
+	void Wrestle ()
+	{
+		beginTime = (float)Time.time;
+		int remainingWrestles = 10;
+		while (remainingWrestles > 0) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				remainingWrestles -= 1;
+			}
+		}
+		wrestleTime = (float)Time.time - beginTime;
+		infection += 0.001f * wrestleTime;
+		isWrestling = false;
+	}
+
+
+
 }
