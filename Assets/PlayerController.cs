3a26ec67d09e2d9c8ebf8358e6537199c59a0a07
CodiakVII <codiak759@gmail.com>
2016-10-15 18:14:48 -0700

    The shit

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
@@ -8,9 +9,20 @@ public class PlayerController : MonoBehaviour {
 	public float movement_v;
 	public float infection;
 	public bool isWrestling;
+	public int lap_num;
+	public int score;
+	public int finalScore;
+	public bool gameEnd;
 
 	public float beginTime;
 	public float wrestleTime;
+
+	public Text infectionText;
+	public Text scoreText;
+	public Text gameOverText;
+	public Text lapText;
+	public Text finalScoreText;
+
 
 
 	private Rigidbody2D rb;
@@ -18,6 +30,14 @@ public class PlayerController : MonoBehaviour {
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		infection = 0.0f;
+		lap_num = 0;
+		SetInfectionText ();
+		gameOverText.text = "";
+		finalScoreText.text = "";
+		SetScoreText ();
+		SetLapText ();
+
 	}
 
 	void FixedUpdate ()
@@ -27,28 +47,40 @@ public class PlayerController : MonoBehaviour {
 			//Wrestle ();
 		//}
 
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			movement_h = 2 * speed;
-			
-		} else if (Input.GetKey (KeyCode.LeftArrow)) {
-			movement_h = -(speed / 5);
+		if (!gameEnd) {
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				movement_h = 2 * speed;
+
+			} else if (Input.GetKey (KeyCode.LeftArrow)) {
+				movement_h = -(speed / 5);
+
+			} else {
+				movement_h = 0.0f;
+			}
+
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				movement_v = 1.5f * speed;
+
+			} else if (Input.GetKey (KeyCode.DownArrow)) {
+				movement_v = -1.5f * speed;
+
+			} else {
+				movement_v = 0.0f;
+			}
+
+			this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (4.0f + movement_h, movement_v);
+
+			score += 1;
+			SetScoreText ();
 
 		} else {
-			movement_h = 0.0f;
+			finalScore = score;
+			infectionText.text = "";
+			lapText.text = "";
+			SetFinalScoreText ();
+			scoreText.text = "";
+			gameOverText.text = "GAME OVER";
 		}
-
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			movement_v = 1.5f * speed;
-
-		} else if (Input.GetKey (KeyCode.DownArrow)) {
-			movement_v = -1.5f * speed;
-
-		} else {
-			movement_v = 0.0f;
-		}
-
-		this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (4.0f + movement_h, movement_v);
-
 	}
 
 	void Wrestle ()
@@ -62,9 +94,30 @@ public class PlayerController : MonoBehaviour {
 		}
 		wrestleTime = (float)Time.time - beginTime;
 		infection += 0.001f * wrestleTime;
+		SetInfectionText ();
 		isWrestling = false;
 	}
 
+	void SetInfectionText ()
+	{
+		string formatted_percentage = infection.ToString () + "%";
+		infectionText.text = "% Infected: " + formatted_percentage;
+	}
+
+	void SetScoreText ()
+	{
+		scoreText.text = "Score: " + score.ToString();
+	}
+
+	void SetLapText ()
+	{
+		lapText.text = "Lap: " + lap_num.ToString();
+	}
+
+	void SetFinalScoreText ()
+	{
+		finalScoreText.text = "Final Score: " + finalScore.ToString();
+	}
 
 
 }
