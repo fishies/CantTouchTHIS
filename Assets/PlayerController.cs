1491b186ae5beedf6d948e93b1d224ed5c6da25d
fishies <Fyshies@dhcp-v014-107.mobile.uci.edu>
2016-10-16 13:51:55 -0700

    Refactored PlayerController.cs
    Infection text now only displays 100% when above 100%. Separated
    physics and non-physics game logic.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,8 +2,8 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
-
+public class PlayerController : MonoBehaviour
+{
 	public float speed;
 	public float movement_h;
 	public float movement_v;
@@ -24,7 +24,7 @@ public class PlayerController : MonoBehaviour {
 
 	void Start ()
 	{
-		rb = GetComponent<Rigidbody2D>();
+		rb = GetComponent<Rigidbody2D> ();
 		infection = 0.0f;
 		lap_num = 0;
 		SetInfectionText ();
@@ -34,46 +34,21 @@ public class PlayerController : MonoBehaviour {
 		SetLapText ();
 	}
 
-	void FixedUpdate ()
+	void Update ()
 	{
 		if (!gameEnd) {
 			if (wigglesRemaining > 0) {
 				infection += 0.125f;
-				if (Input.GetKey (KeyCode.LeftArrow) && wigglesRemaining % 2 == 0) {
+				if (Input.GetKey (KeyCode.LeftArrow) && wigglesRemaining % 2 == 0)
 					wigglesRemaining -= 1;
-				} else if (Input.GetKey (KeyCode.RightArrow) && wigglesRemaining % 2 == 1) {
+				else if (Input.GetKey (KeyCode.RightArrow) && wigglesRemaining % 2 == 1)
 					wigglesRemaining -= 1;
-				}
 			} else if (gameObject.GetComponent<FixedJoint2D> () != null) {
-				Destroy (gameObject.GetComponent<FixedJoint2D>());
+				Destroy (gameObject.GetComponent<FixedJoint2D> ());
 			}
 
-			if (Input.GetKey (KeyCode.RightArrow)) {
-				movement_h = 2 * speed;
-
-			} else if (Input.GetKey (KeyCode.LeftArrow)) {
-				movement_h = -(speed / 5);
-
-			} else {
-				movement_h = 0.0f;
-			}
-
-			if (Input.GetKey (KeyCode.UpArrow)) {
-				movement_v = 1.5f * speed;
-
-			} else if (Input.GetKey (KeyCode.DownArrow)) {
-				movement_v = -1.5f * speed;
-
-			} else {
-				movement_v = 0.0f;
-			}
-
-			rb.velocity = new Vector2 (4.0f + movement_h, movement_v);
-
-			score += 1;
 			SetInfectionText ();
 			SetScoreText ();
-
 		} else {
 			finalScore = score;
 			infectionText.text = "";
@@ -84,30 +59,50 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
+	void FixedUpdate ()
+	{
+		if (!gameEnd) {
+			if (Input.GetKey (KeyCode.RightArrow))
+				movement_h = 2 * speed;
+			else if (Input.GetKey (KeyCode.LeftArrow))
+				movement_h = -(speed / 5);
+			else
+				movement_h = 0.0f;
+
+			if (Input.GetKey (KeyCode.UpArrow))
+				movement_v = 1.5f * speed;
+			else if (Input.GetKey (KeyCode.DownArrow))
+				movement_v = -1.5f * speed;
+			else
+				movement_v = 0.0f;
+
+			rb.velocity = new Vector2 (4.0f + movement_h, movement_v);
+
+			score += 1;
+		}
+	}
+
 	void SetInfectionText ()
 	{
-		infectionText.text = infection.ToString () + "% Infected";
-		/*else {
-			if (wigglesRemaining % 1 == 0)
-				infectionText.text += infectionText + " [PRESS LEFT]";
-			else
-				infectionText.text += " [PRESS RIGHT]";
-		}*/
+		if (infection >= 100.0f)
+			infectionText.text = "100% Infected";
+		else
+			infectionText.text = infection.ToString () + "% Infected";
 	}
 
 	void SetScoreText ()
 	{
-		scoreText.text = "Score: " + score.ToString();
+		scoreText.text = "Score: " + score.ToString ();
 	}
 
 	void SetLapText ()
 	{
-		lapText.text = "Lap: " + lap_num.ToString();
+		lapText.text = "Lap: " + lap_num.ToString ();
 	}
 
 	void SetFinalScoreText ()
 	{
-		finalScoreText.text = "Final Score: " + finalScore.ToString();
+		finalScoreText.text = "Final Score: " + finalScore.ToString ();
 	}
 
 
