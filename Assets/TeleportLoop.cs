be60e1119b39c8cd69bf3ae5e6f4bbcb64ab5df0
fishies <Fyshies@dhcp-v014-107.mobile.uci.edu>
2016-10-16 14:38:56 -0700

    Fixed TeleportLoop.cs

--- Assets/TeleportLoop.cs
+++ Assets/TeleportLoop.cs
@@ -1,64 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TeleportLoop : MonoBehaviour {
+public class TeleportLoop : MonoBehaviour
+{
 
-	public ArrayList RBC;
-	public ArrayList WBC;
-	public ArrayList BAC;
-	public ArrayList spawners;
-    public Transform Spawn;
-    public GameObject spawner_cell;
-    public PlayerController player;
+	public GameObject[] spawners;
+	public Transform Spawn;
+	public GameObject spawner_cell;
+	public PlayerController player;
 	public GameObject player_cell;
 	public bool initial_loop = true;
 
-    public float difficulty_num = 0.25f;
+	public float difficulty_num = 0.25f;
 	// Use this for initialization
-	void Start () {
-		player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ();
 	}
 
-    // Update is called once per frame
-    void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Player"))
-        {
-            player_cell = GameObject.FindGameObjectWithTag("Player");
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.CompareTag ("Player")) {
+			player_cell = GameObject.FindGameObjectWithTag ("Player");
 			if (initial_loop) {
 				spawners = GameObject.FindGameObjectsWithTag ("Initial Spawner");
-				for (var i = 0; i < spawners.Length; i++)
-					Destroy (spawners [i]);
+				foreach (GameObject spawner in spawners)
+					Destroy (spawner);
 				initial_loop = false;
 			}
-			RBC =  GameObject.FindGameObjectsWithTag ("RedBloodCell");
-			for(var i = 0 ; i < RBC.Length ; i ++)
-				Destroy(RBC[i]);
-			RBC =  GameObject.FindGameObjectsWithTag ("WhiteBloodCell");
-			for(var i = 0 ; i < WBC.Length ; i ++)
-				Destroy(WBC[i]);
-			RBC =  GameObject.FindGameObjectsWithTag ("Bacteria");
-			for(var i = 0 ; i < BAC.Length ; i ++)
-				Destroy(BAC[i]);
-            player.wigglesRemaining = 0;
-            Destroy(player.GetComponent<FixedJoint2D>());
-            player_cell.transform.position = new Vector2(0, other.transform.position.y);
-            player.lap_num += 1;
-            player.lapText.text = "Lap: " + player.lap_num;
+			foreach (GameObject obj in Object.FindObjectsOfType<GameObject>()) {
+				if (obj.CompareTag ("RedBloodCell") || obj.CompareTag ("WhiteBloodCell") || obj.CompareTag ("Bacteria"))
+					Destroy (obj);
+			}
+			player.wigglesRemaining = 0;
+			Destroy (player.GetComponent<FixedJoint2D> ());
+			player_cell.transform.position = new Vector2 (0, other.transform.position.y);
+			player.lap_num += 1;
+			player.lapText.text = "Lap: " + player.lap_num;
 
-            InvokeRepeating("spawn_cells", 0f, Random.Range(2.25f, 5.0f));
-            if (player.lap_num % 2 == 0)
-            {
-                diff_num -= 0.25f;
-            }
-        }
-    }
+			InvokeRepeating ("spawn_cells", 0f, Random.Range (2.25f, 5.0f));
+			if (player.lap_num % 2 == 0) {
+				difficulty_num -= 0.25f;
+			}
+		}
+	}
 
-    void spawn_cells()
-    {
-		Instantiate(spawner_cell, Spawn.position, Spawn.rotation);
+	void spawn_cells ()
+	{
+		Instantiate (spawner_cell, Spawn.position, Spawn.rotation);
 
-    }
+	}
 
 }
-
-
