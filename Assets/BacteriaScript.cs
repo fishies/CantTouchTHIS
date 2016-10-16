658599628ce6ad57472b7203a3bb8706e9fc9f6a
fishies <Fyshies@dhcp-v014-107.mobile.uci.edu>
2016-10-15 19:22:01 -0700

    Hot fire mixtape
    Added 4 second music loop, implemented wiggling mechanic

--- Assets/BacteriaScript.cs
+++ Assets/BacteriaScript.cs
@@ -6,6 +6,7 @@ public class BacteriaScript : MonoBehaviour {
 	private Rigidbody2D rb;
 	private float moveMod; //modifies force applied on each FixedUpdate
 	private Vector2 drift; //horizontal offset so we can constantly drift toward left
+	private bool attached;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +14,7 @@ public class BacteriaScript : MonoBehaviour {
 		moveMod = 1.0f;
 		drift.x = -2f;
 		drift.y = 0;
+		attached = false;
 	}
 	
 	// Update is called once per frame
@@ -29,12 +31,15 @@ public class BacteriaScript : MonoBehaviour {
 		}
 		rb.AddForce (drift+moveMod*((Vector2)(target.transform.position-transform.position)).normalized);
     }
-    void OnCollisionEnter2D(Collision2D c)
-    {
-           
-           var joint = gameObject.AddComponent<FixedJoint2D>();
-            joint.connectedBody = c.rigidbody;
-    }
 
-
+    void OnCollisionEnter2D(Collision2D c) {
+		if (!attached) {
+			attached = true;
+			if (c.gameObject.CompareTag ("Player") && c.gameObject.GetComponent<PlayerController> ().infection < 100.0f) {
+				var joint = c.gameObject.AddComponent<FixedJoint2D> ();
+				joint.connectedBody = this.rb;
+				c.gameObject.GetComponent<PlayerController> ().wigglesRemaining += 10;
+			}
+		}
     }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,22 +8,17 @@ public class PlayerController : MonoBehaviour {
 	public float movement_h;
 	public float movement_v;
 	public float infection;
-	public bool isWrestling;
+	public int wigglesRemaining;
 	public int lap_num;
 	public int score;
 	public int finalScore;
 	public bool gameEnd;
-
-	public float beginTime;
-	public float wrestleTime;
 
 	public Text infectionText;
 	public Text scoreText;
 	public Text gameOverText;
 	public Text lapText;
 	public Text finalScoreText;
-
-
 
 	private Rigidbody2D rb;
 
@@ -37,17 +32,23 @@ public class PlayerController : MonoBehaviour {
 		finalScoreText.text = "";
 		SetScoreText ();
 		SetLapText ();
-
 	}
 
 	void FixedUpdate ()
 	{
-
-		//if (isWrestling) {
-			//Wrestle ();
-		//}
-
 		if (!gameEnd) {
+			if (wigglesRemaining > 0) {
+				infection += 0.125f;
+				if (Input.GetKey (KeyCode.LeftArrow) && wigglesRemaining % 2 == 0) {
+					wigglesRemaining -= 1;
+				} else if (Input.GetKey (KeyCode.RightArrow) && wigglesRemaining % 2 == 1) {
+					wigglesRemaining -= 1;
+				}
+			} else if (gameObject.GetComponent<FixedJoint2D> () != null) {
+				
+				Destroy (gameObject.GetComponent<FixedJoint2D>());
+			}
+
 			if (Input.GetKey (KeyCode.RightArrow)) {
 				movement_h = 2 * speed;
 
@@ -68,9 +69,10 @@ public class PlayerController : MonoBehaviour {
 				movement_v = 0.0f;
 			}
 
-			this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (4.0f + movement_h, movement_v);
+			rb.velocity = new Vector2 (4.0f + movement_h, movement_v);
 
 			score += 1;
+			SetInfectionText ();
 			SetScoreText ();
 
 		} else {
@@ -83,25 +85,15 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
-	void Wrestle ()
-	{
-		beginTime = (float)Time.time;
-		int remainingWrestles = 10;
-		while (remainingWrestles > 0) {
-			if (Input.GetKeyDown (KeyCode.Space)) {
-				remainingWrestles -= 1;
-			}
-		}
-		wrestleTime = (float)Time.time - beginTime;
-		infection += 0.001f * wrestleTime;
-		SetInfectionText ();
-		isWrestling = false;
-	}
-
 	void SetInfectionText ()
 	{
-		string formatted_percentage = infection.ToString () + "%";
-		infectionText.text = "% Infected: " + formatted_percentage;
+		infectionText.text = infection.ToString () + "% Infected";
+		/*else {
+			if (wigglesRemaining % 1 == 0)
+				infectionText.text += infectionText + " [PRESS LEFT]";
+			else
+				infectionText.text += " [PRESS RIGHT]";
+		}*/
 	}
 
 	void SetScoreText ()
