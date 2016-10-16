e9260a32bd01f38aad15314be8f962c00ad3a6cf
ThanawatMynaf <tmynaf@uci.edu>
2016-10-15 18:07:16 -0700

    Add files via upload

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
@@ -9,20 +8,9 @@ public class PlayerController : MonoBehaviour {
 	public float movement_v;
 	public float infection;
 	public bool isWrestling;
-	public int lap_num;
-	public int score;
-	public int finalScore;
-	public bool gameEnd;
 
 	public float beginTime;
 	public float wrestleTime;
-
-	public Text infectionText;
-	public Text scoreText;
-	public Text gameOverText;
-	public Text lapText;
-	public Text finalScoreText;
-
 
 
 	private Rigidbody2D rb;
@@ -30,14 +18,6 @@ public class PlayerController : MonoBehaviour {
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody2D>();
-		infection = 0.0f;
-		lap_num = 0;
-		SetInfectionText ();
-		gameOverText.text = "";
-		finalScoreText.text = "";
-		SetScoreText ();
-		SetLapText ();
-
 	}
 
 	void FixedUpdate ()
@@ -47,40 +27,28 @@ public class PlayerController : MonoBehaviour {
 			//Wrestle ();
 		//}
 
-		if (!gameEnd) {
-			if (Input.GetKey (KeyCode.RightArrow)) {
-				movement_h = 2 * speed;
-
-			} else if (Input.GetKey (KeyCode.LeftArrow)) {
-				movement_h = -(speed / 5);
-
-			} else {
-				movement_h = 0.0f;
-			}
-
-			if (Input.GetKey (KeyCode.UpArrow)) {
-				movement_v = 1.5f * speed;
-
-			} else if (Input.GetKey (KeyCode.DownArrow)) {
-				movement_v = -1.5f * speed;
-
-			} else {
-				movement_v = 0.0f;
-			}
-
-			this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (4.0f + movement_h, movement_v);
-
-			score += 1;
-			SetScoreText ();
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			movement_h = 2 * speed;
+			
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			movement_h = -(speed / 5);
 
 		} else {
-			finalScore = score;
-			infectionText.text = "";
-			lapText.text = "";
-			SetFinalScoreText ();
-			scoreText.text = "";
-			gameOverText.text = "GAME OVER";
+			movement_h = 0.0f;
 		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			movement_v = 1.5f * speed;
+
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			movement_v = -1.5f * speed;
+
+		} else {
+			movement_v = 0.0f;
+		}
+
+		this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (4.0f + movement_h, movement_v);
+
 	}
 
 	void Wrestle ()
@@ -94,30 +62,9 @@ public class PlayerController : MonoBehaviour {
 		}
 		wrestleTime = (float)Time.time - beginTime;
 		infection += 0.001f * wrestleTime;
-		SetInfectionText ();
 		isWrestling = false;
 	}
 
-	void SetInfectionText ()
-	{
-		string formatted_percentage = infection.ToString () + "%";
-		infectionText.text = "% Infected: " + formatted_percentage;
-	}
-
-	void SetScoreText ()
-	{
-		scoreText.text = "Score: " + score.ToString();
-	}
-
-	void SetLapText ()
-	{
-		lapText.text = "Lap: " + lap_num.ToString();
-	}
-
-	void SetFinalScoreText ()
-	{
-		finalScoreText.text = "Final Score: " + finalScore.ToString();
-	}
 
 
 }
---
+++ Assets/stickbacteria.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickBacteria : MonoBehaviour {
+
+
+    void OnCollisionEnter(Collision Col)
+    {
+
+        var joint = gameObject.AddComponent<FixedJoint>();
+        joint.connectedBody = Col.rigidbody;
+    }
+}
